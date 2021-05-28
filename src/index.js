b9f9cf38c6e53165bb7eeee2ce7cb561a4e2872b
sarahghossen <ghossensarah@gmail.com>
2021-05-28 17:06:01 -0400

    css changes

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+let principalDate = document.getElementById("principal-date");
+let principalDateCopy = principalDate.cloneNode(true);
+principalDate.appendChild(principalDateCopy);
